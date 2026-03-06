ee5769e9b9f3b0ec339320779652b58f21abf382
kimjuock <bihwaness404@gmail.com>
2026-03-06 19:44:14 +0900

    feat: 수정된 걸로 페이지 연결 시켜두기

--- bwApp/bookwheel/app/book-detail/[bookId]/(top)/gallery.tsx
+++ bwApp/bookwheel/app/book-detail/[bookId]/(top)/gallery.tsx
@@ -1,9 +1,12 @@
 import { router, useLocalSearchParams } from "expo-router";
 import { common } from "@/styles/common";
 import { View, Text, TouchableOpacity } from "react-native";
+import AntDesign from "@expo/vector-icons/build/AntDesign";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Gallery() {
   const { bookId } = useLocalSearchParams<{ bookId: string }>();
+  const groupId = "1";
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -23,6 +26,26 @@ export default function Gallery() {
       >
         <Text style={[common.button, { marginTop: 20 }]}>책사진1</Text>
       </TouchableOpacity>
+
+      {/* 게시물로 생성 */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{ marginLeft: 10 }}
+          onPress={() =>
+            router.push({
+              pathname: "/(tabs)/group/[id]/completed-books",
+              params: { id: groupId },
+            })
+          }
+        >
+          <Ionicons name="add-circle-outline" size={20} color="#333" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- bwApp/bookwheel/app/book-detail/[bookId]/(top)/review.tsx
+++ bwApp/bookwheel/app/book-detail/[bookId]/(top)/review.tsx
@@ -1,9 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { router } from "expo-router";
+import { Ionicons, AntDesign } from "@expo/vector-icons";
 
 export default function Review() {
+  const groupId = "1";
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>책 리뷰</Text>
+    <View
+      style={{
+        flex: 1,
+        padding: 20,
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 18, marginBottom: 15 }}>책 리뷰</Text>
+
+      {/* 게시물 등록 */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{ marginLeft: 10 }}
+          onPress={() =>
+            router.push({
+              pathname: "/(tabs)/group/[id]/completed-books",
+              params: { id: groupId },
+            })
+          }
+        >
+          <AntDesign name="arrows-alt" size={20} color="#333" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
