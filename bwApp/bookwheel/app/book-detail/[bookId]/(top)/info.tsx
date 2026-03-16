d5ced7cda2d8b94fdd47c67e7221ab1d01eb6825
soheesgit <thgml89567@naver.com>
2026-03-16 17:34:11 +0900

    feat: 모임 연동 글씨 marginTop 4 추가

--- bwApp/bookwheel/app/book-detail/[bookId]/(top)/info.tsx
+++ bwApp/bookwheel/app/book-detail/[bookId]/(top)/info.tsx
@@ -100,7 +100,7 @@ export default function Info() {
             {/* 모임 연동 섹션 */}
             <View style={styles.section}>
                 <Text style={styles.groupTitle}>이 책을 읽기로 한 모임이 있어요!</Text>
-                <View style={{ marginTop: 20 }}>
+                <View style={{ marginTop: 10 }}>
                     {data.relatedGroups.map((g) => (
                         <GroupListExtended key={g.id} group={g} />
                     ))}
@@ -117,6 +117,7 @@ const styles = StyleSheet.create({
     },
     section: {
         padding: 20,
+        marginTop: 4,
     },
     sectionTitle: {
         fontSize: 16,
