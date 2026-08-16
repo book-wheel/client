0eeae54967e22325ee5b38261801fa86d90d1497
kimjuock <bihwaness404@gmail.com>
2026-08-16 22:09:38 +0900

    fix: 중복 이동 수정

--- bwApp/bookwheel/app/(tabs)/group/[id]/member-order-edit.tsx
+++ bwApp/bookwheel/app/(tabs)/group/[id]/member-order-edit.tsx
@@ -105,11 +105,6 @@ export default function MemberOrderEdit() {
       });
 
       // 4. 생성된 일정 전체 확인 화면으로 이동
-      router.replace({
-        pathname: "/group/[id]/state",
-        params: { id },
-      });
-
       Alert.alert("저장 완료", "읽기 순서가 저장되고 일정이 생성되었습니다.", [
         {
           text: "확인",
