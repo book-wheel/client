e03326aa74effb1d34109ab02074e750260f64e9
soheesgit <144293475+soheesgit@users.noreply.github.com>
2026-09-07 21:39:59 +0900

    chore: upgrade Expo SDK to 57

--- bwApp/bookwheel/app/auth/(tabs)/_layout.tsx
+++ bwApp/bookwheel/app/auth/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createMaterialTopTabNavigator } from "expo-router/js-top-tabs";
 
 import IdFind from "./idfind";
 import PwFind from "./pwfind";
