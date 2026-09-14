b4fb48beb0a4f092a7fb7c376650c2aa9577be61
Moon So Hee <144293475+soheesgit@users.noreply.github.com>
2026-09-14 20:30:40 +0900

    Revert "chore: upgrade Expo SDK to 57"

--- bwApp/bookwheel/components/groups/GroupsFab.tsx
+++ bwApp/bookwheel/components/groups/GroupsFab.tsx
@@ -53,7 +53,7 @@ export default function GroupsFab({ open, setOpen }: Props) {
 
 const styles = StyleSheet.create({
   overlay: {
-    ...StyleSheet.absoluteFill,
+    ...StyleSheet.absoluteFillObject,
     backgroundColor: "rgba(255,255,255,0.8)",
   },
 
